938c8aca6dc6900e3166c2d9eebf20ca0c6ad3f9
nataliafleitas <nataliafleitas2298@gmail.com>
2020-03-18 19:16:49 -0300

    Agregue contenido, hay errores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Layout from './Layout'
+import Users from '../src/Layout/Pages/Admin/Users/index'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Layout />
+      <Users/>
     </Router>
   )
-  
+
 }
+
 
 export default App
--- src/Layout/Pages/Admin/Users/index.js
+++ src/Layout/Pages/Admin/Users/index.js
@@ -1,4 +1,21 @@
 import React from 'react'
+import './estilos.css';
 import UsersPage from './UsersPage'
 
-export default UsersPage
+class Users extends React.Component{
+  handleClick= position => this.props.onClick(position);
+render(){
+  return(
+      <div  className="contenedor">
+      <h1>Fleitas </h1>
+
+      <div className="imagen">
+
+
+      </div>
+      </div>
+  )
+}
+}
+
+export default Users
