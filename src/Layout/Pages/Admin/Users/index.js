16ce753e20c41456cef1081114ad3cf4568510f7
nataliafleitas <nataliafleitas2298@gmail.com>
2020-03-20 16:53:02 -0300

    Puse iconos y resolvi problemas

--- src/Layout/Pages/Admin/Users/index.js
+++ src/Layout/Pages/Admin/Users/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './estilos.css';
-import UsersPage from './UsersPage'
+import UsersPage from './UsersPage';
+import Imagen from './imagenes/flecha.svg';
+import Imagen1 from './imagenes/descarga.svg';
+import Imagen2 from './imagenes/tacho.svg';
 
 class Users extends React.Component {
   handleClick = position => this.props.onClick(position);
@@ -9,17 +12,26 @@ class Users extends React.Component {
       <div class="contenedor">
         <div className="Rectangle-10">
           <div className="Grupos-Usuarios">
-            <span>Grupos & Usuariosss</span>
+            <span>Grupos & Usuarios</span>
           </div>
+          <div className="Padre-3">
           <div className="Line-3">
+          </div>
           </div>
           <div className="Ipsum-Copy-10">
             <span>Todos los roles</span>
           </div>
-          <div className="Shape">
+          <div className="contfle">
+          <div className="flecha">
+            <img src={Imagen} width="20" height="20" />
           </div>
-          <div className="Rectangle-3">
-            <sapn className="Buscar"> Buscar...</sapn>
+          </div>
+          <div>
+            <input className="Rectangle-3" type="text" name="name" placeholder="Buscar.." />
+          </div>
+          <div>
+          <img src={Imagen1} width="20" height="20" />
+          <img src={Imagen2} width="20" height="20" />
           </div>
         </div>
 
