d40ea99ccbfbacd27da32cf3a51fb0fbf6b8a1c1
nataliafleitas <nataliafleitas2298@gmail.com>
2020-03-20 13:00:13 -0300

    Volvi a pushear

--- src/Layout/Pages/Admin/Users/index.js
+++ src/Layout/Pages/Admin/Users/index.js
@@ -2,30 +2,30 @@ import React from 'react'
 import './estilos.css';
 import UsersPage from './UsersPage'
 
-class Users extends React.Component{
-  handleClick= position => this.props.onClick(position);
-render(){
-  return(
-    <div class="contenedor">
-      <div className="Rectangle-10">
-         <div  className="Grupos-Usuarios">
-            <span>Grupos & Usuarios</span>
-         </div>
-        <div className="Line-3">
+class Users extends React.Component {
+  handleClick = position => this.props.onClick(position);
+  render() {
+    return (
+      <div class="contenedor">
+        <div className="Rectangle-10">
+          <div className="Grupos-Usuarios">
+            <span>Grupos & Usuariosss</span>
+          </div>
+          <div className="Line-3">
+          </div>
+          <div className="Ipsum-Copy-10">
+            <span>Todos los roles</span>
+          </div>
+          <div className="Shape">
+          </div>
+          <div className="Rectangle-3">
+            <sapn className="Buscar"> Buscar...</sapn>
+          </div>
         </div>
-        <div className="Ipsum-Copy-10">
-        <span>Todos los roles</span>
-        </div>
-        <div className="Shape">
-        </div>
-        <div className="Rectangle-3">
-            <sapn  className="Buscar"> Buscar...</sapn>
-        </div>
-      </div>
 
-    </div>
-  )
-}
+      </div>
+    )
+  }
 }
 
 export default Users
