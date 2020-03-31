3b084f4e112f3b910dc845fdd2ba589a8af262be
nataliafleitas <nataliafleitas2298@gmail.com>
2020-03-30 22:57:50 -0300

    Termine interfaz, hace falta el form, y crear los usuarios

--- src/Layout/Pages/Admin/Users/index.js
+++ src/Layout/Pages/Admin/Users/index.js
@@ -9,6 +9,8 @@ import Imagen4 from './imagenes/seleccion.svg';
 import Imagen5 from './imagenes/ajuste.svg';
 import Imagen6 from './imagenes/tacho2.svg';
 import Imagen7 from './imagenes/editar.svg';
+import Imagen8 from './imagenes/flecha1.svg';
+
 
 class Users extends React.Component {
   handleClick = position => this.props.onClick(position);
@@ -43,31 +45,62 @@ class Users extends React.Component {
           </div>
           <div className="desktop_user_groups"></div>
           <div className="Rectangle-5">
-            <div className="Rectangle-2">  <img className="img1"  src={Imagen4} width="24" height="24" />
+            <div className="Rectangle-2">  <img className="img1"  src={Imagen4} width="24" height="24" /> <img className="img1"  src={Imagen8} width="24" height="24" /> <div className="sera"><span className="Grupo-Buenos-Aires">Grupo Buenos Aires</span> </div>
               <div className="AlFondo">
                   <img  className="AlFondo" src={Imagen5} width="24" height="24" />
               </div>
             </div>
 
             <div className="Rectangle-2-Copy ">  <img className="img2"  src={Imagen4} width="24" height="24" />
-            <div className="AlFondo"> <img  className="editar3" src={Imagen7} width="24" height="24" />
+            <div className="AlFondo"> <div className="Rectangle-4"> <span className="No-disponible">No disponible</span> </div><img  className="editar3" src={Imagen7} width="24" height="24" />
                   <img  className="AlFondo2" src={Imagen6} width="24" height="24" />
               </div>
             </div>
             <div>   <img className="img3"  src={Imagen4} width="24" height="24" />
-            <div  className="AlFondo"> <img  className="editar3" src={Imagen7} width="24" height="24" />
+            <div  className="AlFondo"> <div className="Rectangle-4-copy1"> <span className="No-disponible">Disponible</span></div>  <img  className="editarante" src={Imagen7} width="24" height="24" />
             <img  className="AlFondo1" src={Imagen6} width="24" height="24" />
             </div>
             </div>
 
             <div className="Rectangle-2-Copy-copy">  <img className="img4"  src={Imagen4} width="24" height="24" />
-            <div  className="AlFondo"> <img  className="editar3" src={Imagen7} width="24" height="24" />
+            <div  className="AlFondo"> <div className="Rectangle-4-copy2"> <span className="No-disponible">Disponible</span></div><img  className="editar3" src={Imagen7} width="24" height="24" />
+            <img  className="AlFondo" src={Imagen6} width="24" height="24" />
+            </div>
+            </div>
+          </div>
+          <div className="Rectangle-5">
+            <div className="Rectangle-2">  <img className="img1"  src={Imagen4} width="24" height="24" /> <img className="img1"  src={Imagen8} width="24" height="24" /> <div className="sera"><span className="Grupo-Buenos-Aires">Grupo Catamarca</span> </div>
+              <div className="AlFondo">
+                  <img  className="AlFondo" src={Imagen5} width="24" height="24" />
+              </div>
+            </div>
+
+            <div className="Rectangle-2-Copy ">  <img className="img2"  src={Imagen4} width="24" height="24" />
+            <div className="AlFondo"> <div className="Rectangle-4"> <span className="No-disponible">No disponible</span> </div><img  className="editar3" src={Imagen7} width="24" height="24" />
+                  <img  className="AlFondo2" src={Imagen6} width="24" height="24" />
+              </div>
+            </div>
+            <div>   <img className="img3"  src={Imagen4} width="24" height="24" />
+            <div  className="AlFondo"> <div className="Rectangle-4-copy1"> <span className="No-disponible">Disponible</span></div>  <img  className="editarante" src={Imagen7} width="24" height="24" />
+            <img  className="AlFondo1" src={Imagen6} width="24" height="24" />
+            </div>
+            </div>
+
+            <div className="Rectangle-2-Copy-copy">  <img className="img4"  src={Imagen4} width="24" height="24" />
+            <div  className="AlFondo"> <div className="Rectangle-4-copy2"> <span className="No-disponible">Disponible</span></div><img  className="editar3" src={Imagen7} width="24" height="24" />
             <img  className="AlFondo" src={Imagen6} width="24" height="24" />
             </div>
             </div>
           </div>
         </div>
-      </div>
+
+        </div>
+
+
+
+
+
+
 
 
     )
